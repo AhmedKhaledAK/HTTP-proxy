bd557a2581d30cb3d435730a13ff000a80185aad
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-03-28 17:53:05 +0200

    port function

--- http_proxy.py
+++ http_proxy.py
@@ -202,6 +202,17 @@ def http_request_pipeline(source_addr, http_raw_data):
     return None
 
 
+def get_port(searchstr):
+    if searchstr == None:
+        return 80
+    match = re.search(":\d+",searchstr)
+    print("match port:",match)
+    if match != None:
+        print("group port:",match.group()[1:])
+        port = int(match.group()[1:])
+        return port
+    return 80
+
 def parse_http_request(source_addr, http_raw_data) -> HttpRequestInfo:
     """
     This function parses an HTTP request into an HttpRequestInfo
@@ -227,7 +238,10 @@ def parse_http_request(source_addr, http_raw_data) -> HttpRequestInfo:
         method = match.group(1)
         path = match.group(2)
         version = match.group(3)
-    
+
+
+    port = get_port(path)
+        
     headers = http_raw_data[http_raw_data.index('\n')+1:]
     
     headerslist = re.findall(r"([a-zA-Z0-9]+):[^\n\ra-zA-Z/:.0-9]*([a-zA-Z/:.0-9]+)", headers)
@@ -235,11 +249,10 @@ def parse_http_request(source_addr, http_raw_data) -> HttpRequestInfo:
 
     for h in headerslist:
         if h[0].lower().strip() == "host":
-             match = re.search(":\d+",h[1])
-             print("match port:",match)
-             if match != None:
-                 print("group port:",match.group()[1:])
-                 port = int(match.group()[1:])
+            host = h[1]
+            if port == 80:
+                port = get_port(h[1])
+             
     
     """
     headers = http_raw_data[http_raw_data.index('\n')+1:]
